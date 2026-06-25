e78923458dc3c18df1cd5995141909d1259e8ce9
Dir <haidiraditya16@gmail.com>
2026-06-26 04:56:32 +0800

    mantap

---
+++ src/eeee/alamak.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eeee;
+
+/**
+ *
+ * @author k4yah
+ */
+public class alamak {
+    public static void main(String[] args) {
+        System.out.println("halo gais....");
+    }
+}
